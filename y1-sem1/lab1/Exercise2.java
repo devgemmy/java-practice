5a43a853b81a368b0dde04a44738ef6ea3e77334
Anthonia Alexis Asikpo <42866195+devgemmy@users.noreply.github.com>
2025-01-31 23:46:13 +0000

    Create Exercise2.java

---
+++ y1-sem1/lab1/Exercise2.java
@@ -0,0 +1,24 @@
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// long x
+		// the value of x should be a double because it is a decimal
+		double x;
+		
+		x = 100.1;
+		
+		System.out.println(x);
+		
+		x += 10.2; //Line A
+		
+		System.out.println(x);
+		
+		x += 20.3; //Line B
+		
+		System.out.println(x);
+		
+		
+	}
+
+}
