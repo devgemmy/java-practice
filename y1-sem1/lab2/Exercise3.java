2fc1ff1c0df9b9aa13c78e02b0b68649151b2d2a
Anthonia Alexis Asikpo <42866195+devgemmy@users.noreply.github.com>
2025-02-01 21:47:31 +0000

    Create Exercise3.java

---
+++ y1-sem1/lab2/Exercise3.java
@@ -0,0 +1,27 @@
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		int a = 12, b = -7, c = 63;
+		
+		// System.out.println(Math.min(a, b));
+		
+		if (a < b && b < c) {
+			System.out.println("The correct order" + " is " + a + " < " + b + " < " + c);
+		} else if (b < c && c < a) {
+			System.out.println("The correct order" + " is " + b + " < " + c + " < " + a);
+		} else if (c < a && a < b) {
+			System.out.println("The correct order" + " is " + c + " < " + a + " < " + b);
+		} else if (c < b && b < a) {
+			System.out.println("The correct order" + " is " + c + " < " + b + " < " + a);
+		} else if (b < a && a < c) {
+			System.out.println("The correct order" + " is " + b + " < " + a + " < " + c);
+		} else if (a < c && c < b){
+			System.out.println("The correct order" + " is " + a + " < " + c + " < " + b);
+		} else {
+			System.out.println("There is no correct order");
+		}
+
+	}
+
+}
