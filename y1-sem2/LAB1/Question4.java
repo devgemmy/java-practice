89e81986fd058f7c09b75dac4c54dd8e7bd2563c
devgemmy <anthonia@nugitech.com>
2025-04-01 13:03:47 +0100

    question 4 solved

--- y1-sem2/LAB1/Question4.java
+++ y1-sem2/LAB1/Question4.java
@@ -1,40 +1,37 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Objects;
-//import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Question4 {
     public static void main(String[] args) throws IOException {
         FileReader fr = new FileReader("/Users/macbookair/Documents/BRUNEL/YEAR 1/Programming Applications/examquestion4.txt");
         BufferedReader br = new BufferedReader(fr);
 
-        int wordCount = 0;
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        try {
+            String lines;
+            while ((lines = br.readLine()) != null) {
+                // System.out.println(lines);
+                lines = lines.toLowerCase().trim();
+                if (lines.isEmpty()) continue;
 
-        String lines = null;
-        // br.lines().forEach(System.out::println);
-        while ((lines = br.readLine()) != null) {
-            System.out.println(lines.toLowerCase());
-            String[] words = lines.split(" ");
-
-//            for (int i = 0; i < words.length; i++) {
-//                if (Objects.equals(words[i], words[i + 1])) {
-//                    wordCount++;
-//                }
-//            }
-
-            // System.out.println(wordCount);
-
-        }
-
-        // System.out.println(words.length);
+                String[] words = lines.split(" ");
+                for (String word : words) {
+                    word = word.toLowerCase().trim();
+                    wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+                }
+            }
+            for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        } catch (IOException e) { e.printStackTrace(); }
 
         br.close();
         fr.close();
     }
 }
-
 
 
 //
